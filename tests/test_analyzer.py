14bedc2b7b2f376dea1088213f5b08c691d467e1
peijun <contact.zouri.music@gmail.com>
2020-04-30 02:10:21 +0900

    add test for analyzer

---
+++ tests/test_analyzer.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+import auderch.analyzer as analyzer
+from numpy.testing import assert_array_equal
+import numpy as np
+
+
+class TestAnalyzer(unittest.TestCase):
+
+    def test_open_wavefile(self):
+        filepath = os.getcwd() + '/test.wav'
+        file, rate = analyzer.open_wavfile(filepath)
+
+        exwavrate = 8000
+
+        self.assertEquals(exwavrate, rate)
+
+    def test_transform_nparray(self):
+        wavfile, rate = analyzer.open_wavfile("test.wav")
+        narray, narray_frame = analyzer.transform_nparray(wavfile)
+        a = 1  # 振幅
+        fs = 8000  # サンプリング周波数
+        f0 = 440  # 周波数
+        sec = 5  # 秒
+
+        swav = []
+
+        for n in np.arange(fs * sec):
+            # サイン波を生成
+            s = a * np.sin(2.0 * np.pi * f0 * n / fs)
+            swav.append(s)
+
+        swav = [int(x * 32767.0) for x in swav]
+
+        assert_array_equal(swav, narray)
+
+
+if __name__ == "__main__":
+    unittest.main()
