caed1e7e52b27d0549902f80f18fab09e335506b
peijun <contact.zouri.music@gmail.com>
2020-06-08 23:46:52 +0900

    del no use comment

--- auderch/analyzer.py
+++ auderch/analyzer.py
@@ -86,11 +86,3 @@ def peak_to_landmark(peaks_freq, peaks_time, target_freq=10, target_time=10, tar
             landmarks.append((hsh, anc_time))
 
     return landmarks
-
-
-"""
-main_wave, main_wave_rate = open_wavfile()
-array, frames = transform_nparray(main_wave)
-pf, pt = find_peak(array, frames, 5)
-list_landmark = peak_to_landmark(pf,pt)
-"""
