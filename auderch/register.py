e80ba10f30b921b551ea6b1e1bd9e85fd1e9e953
peijun <contact.zouri.music@gmail.com>
2020-06-13 00:30:10 +0900

    add register.py

---
+++ auderch/register.py
@@ -0,0 +1,23 @@
+import auderch.analyzer as aa
+from pathlib import Path
+
+
+def register_one(path, imongodb):
+    print("Please enter a unique song ID as an integer")
+    id = int(input())
+
+    list_landmark = aa.analyzer(path)
+
+    for landmark in list_landmark:
+        imongodb.insert(id, int(landmark[0]), int(landmark[1]))
+
+
+def register_directory(imongodb):
+    print("Please enter the path to the directory where the wave file you want to register is located.")
+
+    dirpath = input()
+
+    p = Path(dirpath)
+
+    for filepath in p.glob("*.wav"):
+        register_one(filepath, imongodb)
