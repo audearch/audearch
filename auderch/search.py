e4aa41820fe82238a1a48b972bf6106c1a1ade77
peijun <contact.zouri.music@gmail.com>
2020-06-13 02:41:30 +0900

    add search.py

---
+++ auderch/search.py
@@ -0,0 +1,20 @@
+import auderch.analyzer as aa
+import statistics
+
+
+def search(path, imongodb):
+    ids = []
+
+    list_landamrk = aa.analyzer(path)
+
+    for landmark in list_landamrk:
+        cur = imongodb.find(filter={'music_hash': int(landmark[0])})
+        for doc in cur:
+            ids.append(doc['music_id'])
+
+    ans = statistics.mode(ids)
+
+    print("RESULT")
+    print("-----------------------")
+    print("id:" + str(ans))
+    print("-----------------------")
