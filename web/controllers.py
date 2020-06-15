ca0f1f7b1b1ae4801330faa5a50367e1d86f858d
peijun <contact.zouri.music@gmail.com>
2020-06-15 21:41:03 +0900

    add upload function

--- web/controllers.py
+++ web/controllers.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from auderch.analyzer import analyzer
+from auderch.database import MongodbFactory
+from fastapi import FastAPI, File, UploadFile
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
+import time
+
 
 app = FastAPI(
     title='auderch',
@@ -14,3 +18,24 @@ jinja_env = templates.env
 
 def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.post("/upload_file/", status_code=201)
+async def upload_file(files: UploadFile = File(...)):
+    mongodb = MongodbFactory()
+    imongo = mongodb.create()
+
+    id = int(str(time.time())[-6:])
+
+    file_object = files.file
+    list_landmark = analyzer(file_object)
+
+    for landmark in list_landmark:
+        imongo.insert(id, int(landmark[0]), int(landmark[1]))
+
+    return {"music_id": id}
+
+
+@app.get('/upload')
+def upload(request: Request):
+    return templates.TemplateResponse('upload.html', {'request': request})
--- web/urls.py
+++ web/urls.py
@@ -1,3 +1,4 @@
-from web.controllers import app, index
+from web.controllers import app, index, upload, upload_file
 
 app.add_api_route('/', index)
+app.add_api_route('/upload', upload)
