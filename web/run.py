f0ee5d992d7e39ed6c95ce76acb80492d7f9390f
peijun <contact.zouri.music@gmail.com>
2020-06-19 23:25:55 +0900

    del web-system

--- web/run.py
+++
@@ -1,6 +0,0 @@
-from web.urls import app
-import uvicorn
-
-if __name__ == '__main__':
-
-    uvicorn.run(app=app)
--- web/urls.py
+++
@@ -1,4 +0,0 @@
-from web.controllers import app, index, upload, upload_file
-
-app.add_api_route('/', index)
-app.add_api_route('/upload', upload)
