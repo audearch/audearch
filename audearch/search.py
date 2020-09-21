bc09347f7dba89c4085cb9257c8a6ec90d0e861c
peijun <contact.zouri.music@gmail.com>
2020-09-22 00:37:48 +0900

    feat:(draft)search algorithm v0.1.0
    連続したハッシュ検索（まだ作業段階）

--- audearch/search.py
+++ audearch/search.py
@@ -7,10 +7,21 @@ def search(path: str, size: int, imongodb):
 
     list_landamrk = aa.analyzer(path, size)
 
-    for landmark in list_landamrk:
-        cur = imongodb.find_music(filter={'music_hash': int(landmark[0])})
-        for doc in cur:
-            ids.append(doc['music_id'])
+    for i, landmark in enumerate(list_landamrk):
+        id1, id2, id3 = [], [], []
+
+        cur1 = imongodb.find_music(filter={'music_hash': int(landmark[0])})
+        cur2 = imongodb.find_music(filter={'music_hash': int(list_landamrk[i+1][0])})
+        cur3 = imongodb.find_music(filter={'music_hash': int(list_landamrk[i+2][0])})
+
+        for doc1, doc2, doc3 in zip(cur1, cur2, cur3):
+
+            id1.append(doc1['music_id'])
+            id2.append(doc2['music_id'])
+            id3.append(doc3['music_id'])
+
+        id_common_123 = list(set(id1) & set(id2) & set(id3))
+        ids.extend(id_common_123)
 
     ans = statistics.mode(ids)
 
